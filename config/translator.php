2f0872839783170fe0733c8264611175485edb45
Morten Poul Jensen <mpj@signifly.com>
2019-03-07 11:51:33 +0100

    Update config

--- config/translator.php
+++ config/translator.php
@@ -3,13 +3,22 @@
 return [
 
     /*
-     * The default language code.
+     * The default language code that is used by the package
+     * to make comparisons against other languages
+     * in order to provide statistics.
      */
     'default_language_code' => 'en',
 
     /*
-     * The translation model.
+     * This is the name of the table that will be created by the migration and
+     * used by the Translation model shipped with this package.
      */
-    'translation_model' => \Signifly\Translatable\Models\Translation::class,
+    'table_name' => 'translations',
+
+    /*
+     * This model will be used to store translations.
+     * It should either be or extend the Signifly\Translator\Models\Translation model.
+     */
+    'translation_model' => \Signifly\Translator\Models\Translation::class,
 
 ];
